34958161a24f99fb0e18e4a55342bcef2f3da1d7
Rainer Wagner-Ballner <rainer.wagner-ballner@alphasystems.com>
2021-10-20 14:21:03 +0200

    First Tailwind changes

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,15 +1,19 @@
 <template>
-  <div class="layout">
-    <header class="header">
-      <strong>
-        <g-link to="/">{{ $static.metadata.siteName }}</g-link>
-      </strong>
-      <nav class="nav">
-        <g-link class="nav__link" to="/">Home</g-link>
-        <g-link class="nav__link" to="/about/">About</g-link>
-        <g-link class="nav__link" to="/cms/">CMS</g-link>
-        <g-link class="nav__link" to="/links/">Links</g-link>
-      </nav>
+  <div class="layout bg-base">
+    <header class="header bg-secondary">
+      <div class="container flex mx-auto justify-center">
+        <strong>
+          <g-link class="text-accent" to="/">{{ $static.metadata.siteName }}</g-link>
+        </strong>
+      </div>
+      <div class="container mx-auto">
+        <nav class="nav flex justify-evenly">
+          <g-link class="nav__link" to="/">Home</g-link>
+          <g-link class="nav__link" to="/about/">About</g-link>
+          <g-link class="nav__link" to="/cms/">CMS</g-link>
+          <g-link class="nav__link" to="/links/">Links</g-link>
+        </nav>
+      </div>
     </header>
     <slot/>
   </div>
@@ -17,36 +21,14 @@
 
 <static-query>
 query {
-  metadata {
-    siteName
-  }
+metadata {
+siteName
+}
 }
 </static-query>
 
 <style>
-body {
-  font-family: -apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif;
-  margin:0;
-  padding:0;
-  line-height: 1.5;
-}
-
-.layout {
-  max-width: 760px;
-  margin: 0 auto;
-  padding-left: 20px;
-  padding-right: 20px;
-}
-
-.header {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin-bottom: 20px;
-  height: 80px;
-}
-
 .nav__link {
-  margin-left: 20px;
+  @apply text-accent
 }
 </style>
