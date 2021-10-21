2276437188ed83f43e90a5672c9741e1073094ce
Rainer Wagner-Ballner <rainer.wagner-ballner@alphasystems.com>
2021-10-21 12:51:27 +0200

    Failed attempt at importing data

--- src/pages/FreeCompany.vue
+++ src/pages/FreeCompany.vue
@@ -1,17 +1,25 @@
 <template>
   <Layout>
-    <PageTitle>Rainbow Riot</PageTitle>
+    <PageTitle>
+      Rainbow Riot
+    </PageTitle>
     <p>Information about our FC will go here.</p>
   </Layout>
 </template>
 
 <script>
 import PageTitle from "../components/PageTitle";
+
 export default {
   components: {PageTitle},
   metaInfo: {
     title: "FC"
-  }
+  },
+  data(){
+    return{
+      fcInfo: null
+    }
+  },
 }
 </script>
 
