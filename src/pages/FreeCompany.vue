51477bcb6ccaba0ba6cc8a8bf543e54b9f751f52
ShoMinamimoto <ceruleanpaladin@gmail.com>
2021-10-26 19:20:09 +0200

    FC layered image test

--- src/pages/FreeCompany.vue
+++ src/pages/FreeCompany.vue
@@ -1,11 +1,24 @@
+<!--suppress GraphQLUnresolvedReference, HtmlRequiredAltAttribute -->
 <template>
   <Layout>
-    <PageTitle>
-      Rainbow Riot
-    </PageTitle>
-    <p>Information about our FC will go here.</p>
+    <div class="relative">
+      <PageTitle class="mr-10 text-center">
+        Rainbow Riot
+      </PageTitle>
+      <div class="relative h-24 w-24 mx-auto">
+        <img class="absolute inset-0" :src="$static.allFreeCompany.edges[0].node.Crest[0]"/>
+        <img class="absolute inset-0" :src="$static.allFreeCompany.edges[0].node.Crest[1]"/>
+        <img class="absolute inset-0" :src="$static.allFreeCompany.edges[0].node.Crest[2]"/>
+      </div>
+      <p class="text-center">Information about our FC will go here.</p></div>
   </Layout>
 </template>
+
+<static-query>
+query { allFreeCompany{ edges{ node{
+  Crest
+}}}}
+</static-query>
 
 <script>
 import PageTitle from "../components/PageTitle";
