24f73a5bc9bfb2581bc1ba33a1001dd48c37a64e
ShoMinamimoto <ceruleanpaladin@gmail.com>
2021-10-31 18:59:08 +0100

    Tailwind adjust Link headings

--- src/components/LinkCategory.vue
+++ src/components/LinkCategory.vue
@@ -2,12 +2,12 @@
 <template>
   <div>
     <div v-for="category in $static.allLinks.edges" class="text-center">
-      <div class="mx-auto border-2 w-1/3 rounded-xl bg-secondary text-accent text-xl py-2 m-4">
+      <div class="mx-auto border-2 w-1/3 rounded-xl bg-secondary text-accent text-xl font-semibold py-2 m-4">
         <span>{{ category.node.name }}</span>
       </div>
       <div class="grid grid-cols-3 justify-between justify-items-stretch gap-4">
         <div v-for="subcat in category.node.subcats">
-          <div class="border-2 border-white rounded-2xl mb-2"><span>{{ subcat.name }}</span></div>
+          <div class="border-2 border-white rounded-2xl mb-2"><span class="text-lg font-semibold">{{ subcat.name }}</span></div>
           <div class="flex">
             <div class="self-center">
               <g-image class="border border-black ring-2 ring-xivring rounded-xl shadow-lg" :src="subcat.image"/>
