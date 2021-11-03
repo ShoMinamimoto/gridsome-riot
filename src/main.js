97483a87c01bf406ceb5bb62bc47459d28c2768c
ShoMinamimoto <ceruleanpaladin@gmail.com>
2021-11-03 21:03:22 +0100

    Gridsome add Font Awesome

--- src/main.js
+++ src/main.js
@@ -2,8 +2,18 @@
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 
 import DefaultLayout from '~/layouts/Default.vue'
+import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
+import {config, library} from "@fortawesome/fontawesome-svg-core";
+import {fab} from "@fortawesome/free-brands-svg-icons";
+import {fas} from "@fortawesome/free-solid-svg-icons";
+import {far} from "@fortawesome/free-regular-svg-icons";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+
+config.autoAddCss = false;
+library.add(fab, fas, far);
 
 export default function (Vue, { router, head, isClient }) {
   // Set default layout as a global component
-  Vue.component('Layout', DefaultLayout)
+  Vue.component('Layout', DefaultLayout);
+  Vue.component('font-awesome', FontAwesomeIcon);
 }
